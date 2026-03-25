c1719f0f14474b7ba69d995c6fa01748bc301b10
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-25 16:45:31 +0000

    add planet db model

--- src/models.py
+++ src/models.py
@@ -8,9 +8,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(
-        String(120, unique=True, nullable=False))
-    first_name: Mapped[str] = mapped_column(String(120, nullable=False))
-    last_name_name: Mapped[str] = mapped_column(String(120, nullable=False))
+        String(120), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
@@ -25,3 +25,11 @@ class User(db.Model):
             "last_name": self.last_name_name
             # do not serialize the password, its a security breach
         }
+
+
+class Planet(db.Model):
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[int] = mapped_column(Integer(), nullable=True)
+    climate: Mapped[str] = mapped_column(String(120), nullable=True)
