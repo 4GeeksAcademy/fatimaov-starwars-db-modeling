de25b7d97d342bd252cd299d1ab403b4d74e9de7
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-25 16:40:37 +0000

    add user db model

--- src/models.py
+++ src/models.py
@@ -1,19 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(120, unique=True, nullable=False))
+    first_name: Mapped[str] = mapped_column(String(120, nullable=False))
+    last_name_name: Mapped[str] = mapped_column(String(120, nullable=False))
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name_name
             # do not serialize the password, its a security breach
         }
